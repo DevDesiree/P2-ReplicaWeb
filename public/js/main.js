aeb9e403ee9bd49809fc44f29ecf85e1036110bd
Desiree <desireesanchezdev@gmail.com>
2023-11-15 18:56:16 +0100

    Add animation image banner

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,33 @@
-var extraLink = document.getElementById('extra');
-var menu = document.getElementById('extra_menu_options');
 
-extraLink.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (menu.style.display === 'none' || menu.style.display === '') {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
+window.addEventListener('scroll', function () {
+    const sectionIds = ['description', 'features', 'screens', 'extras'];
+    const menuLinks = document.querySelectorAll('nav ul li a'); 
+
+    sectionIds.forEach((sectionId, index) => {
+        const section = document.getElementById(sectionId);
+
+        if (section) {
+            const top = section.offsetTop;
+            const height = section.clientHeight;
+
+            if (window.scrollY >= top && window.scrollY < top + height) {
+
+                if (menuLinks[index] === menuLinks[2]) {
+                    menuLinks[index].classList.add('active');
+                } else {
+                    menuLinks[index].classList.add('active');
+                    menuLinks[index].textContent = sectionId.toUpperCase();        
+                }
+
+                
+            } else {
+                menuLinks[index].classList.remove('active');
+                menuLinks[index].textContent = sectionId.toUpperCase();
+
+            }
+        }
+    });
 });
-
-document.addEventListener('click', function (event) {
-    if (!extraLink.contains(event.target) && !menu.contains(event.target)) {
-        menu.style.display = 'none';
-    }
-});
-
 
 
 
